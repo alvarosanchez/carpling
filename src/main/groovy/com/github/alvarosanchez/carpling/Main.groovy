1cacc7fc059210906632ecc155d893fccfca56b2
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2014-01-28 00:53:04 +0100

    Changed variables format

--- src/main/groovy/com/github/alvarosanchez/carpling/Main.groovy
+++ src/main/groovy/com/github/alvarosanchez/carpling/Main.groovy
@@ -33,8 +33,8 @@ class Main {
             browser.with {
                 go()
 
-                $('#login_form').user_email_login = System.getProperty('carpling.email')
-                $('#login_form').user_password_login = System.getProperty('carpling.password')
+                $('#login_form').user_email_login = System.getProperty('carpling_email')
+                $('#login_form').user_password_login = System.getProperty('carpling_password')
 
                 $('#login').click()
 
@@ -90,7 +90,7 @@ class Main {
 
             }
 
-            sendMail(System.getProperty('email.to').split(','), 'Carpling', foundTrips.toString())
+            sendMail(System.getProperty('email_to').split(','), 'Carpling', foundTrips.toString())
 
             browser.close()
             System.exit(0)
@@ -102,11 +102,11 @@ class Main {
 
     static DB connectDB() {
         def config = [
-                server: System.getProperty('mongo.server'),
-                port: System.getProperty('mongo.port'),
+                server: System.getProperty('mongo_server'),
+                port: System.getProperty('mongo_port'),
                 database: "carpling",
-                username: System.getProperty('mongo.username'),
-                password: System.getProperty('mongo.password')
+                username: System.getProperty('mongo_username'),
+                password: System.getProperty('mongo_password')
         ]
 
         def uri = "mongodb://${config.username}:${config.password}@${config.server}:${config.port}/${config.database}"
@@ -134,8 +134,8 @@ class Main {
         def transport = session.getTransport "smtps"
 
         def host = 'smtp.gmail.com'
-        def username = System.getProperty('email.username')
-        def password = System.getProperty('email.password')
+        def username = System.getProperty('email_username')
+        def password = System.getProperty('email_password')
 
         try {
             transport.connect (host, username, password)
